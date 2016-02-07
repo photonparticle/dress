eb93e84bcb5f5946fd95e8f5237b1572a5d0e14e
photonparticle <atroxdiesel@yahoo.com>
2016-02-07 17:07:06 +0200

    Relacii++

--- public/themes/administration/views/products_partials/product_relationships_tab.blade.php
+++ public/themes/administration/views/products_partials/product_relationships_tab.blade.php
@@ -15,6 +15,52 @@
             </select>
         </div>
         <div class="clearfix"></div>
+        
+        
+
+        <div class="col-xs-12 margin-top-20 categories-container">
+            <label for="manufacturers" class="control-label col-xs-12 default no-padding">
+                {{trans('products.manufacturers')}}
+            </label>
+            <select id="manufacturers" 
+                    name="manufacturers[]" 
+                    class="form-control select2me input-lg no-padding" 
+                    
+                    
+
+                    data-placeholder="{{trans('products.select_manufacturers')}}">
+                @if(isset($categories) && is_array($categories))
+                    @foreach($categories as $key => $category)
+                        <option value="{{$category['id']}}" @if(!empty($related_categories) && in_array($category['id'], $related_categories)) selected="selected" @endif>{{$category['title']}}</option>
+                    @endforeach
+                @endif
+            </select>
+        </div>
+        <div class="clearfix"></div>
+        
+        
+        
+        <div class="col-xs-12 margin-top-20 categories-container">
+            <label for="related_products" class="control-label col-xs-12 default no-padding">
+                {{trans('products.related_products')}}
+            </label>
+            <select id="related_products" 
+                    name="related_products[]" 
+                    class="form-control select2me input-lg no-padding" 
+                    
+                    multiple="multiple"
+
+                    data-placeholder="{{trans('products.related_products')}}">
+                @if(isset($categories) && is_array($categories))
+                    @foreach($categories as $key => $category)
+                        <option value="{{$category['id']}}" @if(!empty($related_categories) && in_array($category['id'], $related_categories)) selected="selected" @endif>{{$category['title']}}</option>
+                    @endforeach
+                @endif
+            </select>
+        </div>
+        <div class="clearfix"></div>
+        
+              
 
         <div class="margin-top-20">
             <button class="btn green-haze save_product">
